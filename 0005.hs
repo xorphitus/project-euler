178df436b94bf2259ac390e0e72b460fb1120058
xorphitus <xorphitus@gmail.com>
2017-08-02 01:56:49 +0900

    WIP 5

--- 0005.hs
+++ 0005.hs
@@ -1,5 +1,7 @@
 -- WIP
 
+import Data.List
+
 {-|
  2 = 2^1
  3 =     3^1
@@ -36,21 +38,52 @@ isPrime x =
 primes :: Int -> [Int]
 primes x = filter isPrime [2..x]
 
--- TODO
+-- >>> countPrime 3 3
+-- 1
+-- >>> countPrime 9 3
+-- 2
+-- >>> countPrime 18 3
+-- 2
 countPrime :: Int -> Int -> Int
-countPrime n p = n
+countPrime n p =
+  let factors = map fst
+        $ filter ((== 0) . (rem n) . snd)
+        $ takeWhile (\x -> snd x <= n)
+        $ map (\x -> (x, p ^ x)) [1..]
+  in case factors of
+    [] -> 0
+    xs -> last xs
 
+-- >>> decomposeToPrimes 3
+-- [(3, 1)]
+-- >>> decomposeToPrimes 12
+-- [(2, 2), (3, 1)]
+-- >>> decomposeToPrimes 90
+-- [(2, 1), (3, 2), (5, 1)]
 decomposeToPrimes :: Int -> [(Int, Int)]
 decomposeToPrimes x =
   let p = primes x
   in map (\n -> (n, (countPrime x n))) p
 
--- TODO
-aggregatePrimes :: [[(Int, Int)]] -> Int
+-- >>> aggregatePrimes [[(2, 1), (3, 2)], [(2, 3), (5, 1)], [(2, 1), (3, 1)]]
+-- [(2, 3), (3, 2), (5, 1)]
+aggregatePrimes :: [[(Int, Int)]] -> [(Int, Int)]
 aggregatePrimes xss =
   let flatten = foldr (++) []
-      flattened = flatten xss
-  in 10
+      order = \x y ->
+        let o = compare (fst x) (fst y)
+        in case o of
+          EQ -> compare (snd x) (snd y)
+          e  -> e
+  in map last
+    $ groupBy (\x y -> fst x == fst y)
+    $ sortBy order
+    $ flatten xss
+
+-- >>> multiply [(2, 1), (3, 2)]
+-- 18
+multiply :: [(Int, Int)] -> Int
+multiply xs = foldr (*) 1 $ map (\x -> (fst x) ^ (snd x)) xs
 
 main :: IO ()
-main = print $ aggregatePrimes (map decomposeToPrimes [1..20])
+main = print $ multiply $ aggregatePrimes (map decomposeToPrimes [1..20])
