94f7a8dbd249424d8be90eb222562173eeebb6fd
xorphitus <xorphitus@gmail.com>
2017-08-22 13:06:17 +0900

    枝刈り

--- 0012.hs
+++ 0012.hs
@@ -1,4 +1,4 @@
--- WIP
+import Data.List
 
 triangle :: Int -> Int
 triangle x =
@@ -6,11 +6,12 @@ triangle x =
   in floor((1 + x') * x' / 2)
 
 divisors :: Int -> [Int]
-divisors x = filter (\y -> rem x y == 0) [1..x]
+divisors x =
+  let divs = filter (\y -> rem x y == 0) [2..(floor (fromIntegral x / 2))]
+  in [1, x] ++ divs
 
 main :: IO ()
 main = print
-  $ head
-  $ dropWhile (\(_, l) -> l > 500)
+  $ find (\(_, l) -> l > 500)
   $ map (\x -> (x, length $ divisors x))
-  $ map triangle [1..]
+  $ map triangle [3..]
