cf00ead6d535042d1a77874684c9b9aadbfa322e
xorphitus <xorphitus@gmail.com>
2017-08-22 14:23:54 +0900

    solve 12

--- 0012.hs
+++ 0012.hs
@@ -5,13 +5,16 @@ triangle x =
   let x' = fromIntegral x
   in floor((1 + x') * x' / 2)
 
-divisors :: Int -> [Int]
-divisors x =
-  let divs = filter (\y -> rem x y == 0) [2..(floor (fromIntegral x / 2))]
-  in [1, x] ++ divs
+-- 総当たりでカウントしてると計算が終わらない
+countDivisors :: Int -> Int
+countDivisors x =
+  let n = ceiling (sqrt $ fromIntegral x)
+      divs = filter (\y -> rem x y == 0) [1..n]
+      divlen = length divs
+  in if (last divs) == n then divlen * 2 - 1 else divlen * 2
 
 main :: IO ()
 main = print
   $ find (\(_, l) -> l > 500)
-  $ map (\x -> (x, length $ divisors x))
+  $ map (\x -> (x, countDivisors x))
   $ map triangle [3..]
