77a3c4fa27ca3b0e75867ace5fa5d5e9e87d702a
xorphitus <xorphitus@gmail.com>
2017-08-29 21:40:17 +0900

    WIP 0014

--- 0014.hs
+++ 0014.hs
@@ -1,24 +1,21 @@
 -- WIP
+-- 遅すぎて終わらないかもver
 
 limit :: Int
 limit = 1000000
-
-powsOf2 :: [Int]
-powsOf2 = takeWhile (< limit) $ map (2 ^) [0..]
-
-log2 :: Int -> Int
-log2 = floor . logBase 2 . fromIntegral
 
 collatz :: Int -> Int
 collatz n
   | odd n     = 3 * n + 1
   | otherwise = floor (fromIntegral n / 2)
 
-collatzSeq :: [Int] -> [Int]
-collatzSeq []  = []
-collatzSeq (x:xs)
-  | x == 1    = reverse (x:xs)
-  | otherwise = collatzSeq (collatz x : x : xs)
+collatzSeq :: Int -> [Int]
+collatzSeq n = toSeq [n]
+  where
+    toSeq []  = []
+    toSeq (x:xs)
+      | x == 1    = reverse (x:xs)
+      | otherwise = toSeq (collatz x : x : xs)
 
 main :: IO ()
-main = print $ collatzSeq [1]
+main = print $ maximum $ map (length . collatzSeq) [1..limit]
